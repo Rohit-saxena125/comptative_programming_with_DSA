279b18465fbb52b4d3758233e98e08ef8d4a17dd
Rohit saxena <75602589+Rohit-saxena125@users.noreply.github.com>
2022-04-03 14:20:34 +0530

    Delete Machine Learning directory

--- Machine Learning/Bubblesort.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-def Bubble_sort(arr) :
-    for i in range(len(arr)) :
-        for j in range(len(arr)-1) :
-            if arr[j] > arr[j+1] :
-                arr[j], arr[j+1] = arr[j+1], arr[j]
-    return arr
-np.array(Bubble_sort([5,8,9,4,6,1,2,3,7]))
--- Machine Learning/First.py
+++
@@ -1,2 +0,0 @@
-import tenserflow as tf
-tf.placeholder(tf.float32, [None, 784])
